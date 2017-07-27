7be3efa9abcf8897dbc80c32a09ea8893e6f2cc1
cesar <cesar@cesar>
2017-07-26 20:07:35 -0600

    Segunda modificacion y pruebas con Git

--- Herramienta de Alto Nivel/src/herramienta/de/alto/nivel/HerramientaDeAltoNivel.java
+++ Herramienta de Alto Nivel/src/herramienta/de/alto/nivel/HerramientaDeAltoNivel.java
@@ -17,6 +17,8 @@ public class HerramientaDeAltoNivel {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hello World");
+        for (int i = 0; i < 5; i++)
+            System.out.println(i);
     }
     
 }
